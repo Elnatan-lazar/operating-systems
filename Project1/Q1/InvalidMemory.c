a47e5530b5db7f87c363c45862fdbe6f48ecb20d
noaShtrasberg <155996037+noaShtrasberg@users.noreply.github.com>
2025-06-17 10:40:32 +0300

    little change

--- Project1/Q1/InvalidMemory.c
+++ Project1/Q1/InvalidMemory.c
@@ -2,11 +2,8 @@
 #include <stdio.h>
 
 int main() {
-    int *ptr = (int *)0xDEADBEEF; // כתובת לא חוקית בזיכרון
-    int value = *ptr; // ניסיון קריאה – תגרום לשגיאה/קריסה
+    int *ptr = (int *)0xDEADBEEF; // Invalid memory
+    int value = *ptr; // Will make the error
     printf("Value: %d\n", value);
     return 0;
 }
-//
-// Created by elnatan on 20/04/2025.
-//
